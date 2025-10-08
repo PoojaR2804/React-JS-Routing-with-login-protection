a2034dc2000c73a7149fccae768a4362691a2ca1
Pooja.R <poojaramadhas546@gmail.com>
2025-10-08 20:45:24 +0530

    ProtectedRoute.js

---
+++ ProtectedRoute.js
@@ -0,0 +1,10 @@
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = localStorage.getItem('auth');
+  return isAuthenticated === 'true' ? children : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
+
+
